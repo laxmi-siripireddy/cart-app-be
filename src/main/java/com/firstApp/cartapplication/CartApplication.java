3e244a230ebc6b88294928a5837baa1108fde375
Mahalaxmi-thrymr <mahalakshmi.sirepireddy@thrymr.net>
2019-11-11 18:03:02 +0530

    first commit

---
+++ src/main/java/com/firstApp/cartapplication/CartApplication.java
@@ -0,0 +1,13 @@
+package com.firstApp.cartapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApplication.class, args);
+	}
+
+}
